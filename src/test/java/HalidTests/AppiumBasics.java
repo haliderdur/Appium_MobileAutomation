82e47ef778fd21b749d95f664debb63fd6253c6a
Halid Erdur <haliderdur@gmail.com>
2024-02-29 11:19:24 +0100

    set UiAutomator options set Android driver

--- src/test/java/HalidTests/AppiumBasics.java
+++ src/test/java/HalidTests/AppiumBasics.java
@@ -1,30 +1,49 @@
 package HalidTests;
 
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 public class AppiumBasics {
 
     @Test
-    public void AppiumTest() throws MalformedURLException, URISyntaxException {
+    public void AppiumTest() throws URISyntaxException, MalformedURLException {
+
+        String mainJS_FilePath = "C:\\Users\\halid\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+        String ipAddress = "127.0.0.1";
+        int port = 4723;
+
+        // start server
+        AppiumDriverLocalService service = new AppiumServiceBuilder()
+                .withAppiumJS(new File(mainJS_FilePath))
+                .withIPAddress(ipAddress)
+                .usingPort(port)
+                .withArgument(GeneralServerFlag.BASEPATH, "/wd/hub/")
+                .build();
+        service.start();
 
         // AndroidDriver , IOSDriver
         // Appium code --> Appium Server --> Mobile
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("HalidAndroidDevice"); // emulator
-        options.setApp("C:\\Users\\halid\\IdeaProjects\\Appium_MobileAutomation\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+        options.setDeviceName("HalidAndroidDevice"); // emulator name must match with virtual device name
+        options.setApp("C:\\Users\\halid\\IdeaProjects\\Appium_MobileAutomation\\src\\test\\java\\resources\\ApiDemos-debug.apk"); // install the app from pc into the mobile device
 
+        AndroidDriver driver = new AndroidDriver(new URI("http://" + ipAddress + ":" + port + "/wd/hub").toURL(), options);
+        driver.quit();
 
-        AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+        // stop server
+        service.stop();
 
         // Actual automation
     }
-
 
 }
